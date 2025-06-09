ce3e3630d8d6462da4b1c195298bf5cd9f46087b
Pablo <63043113+Pacopogo@users.noreply.github.com>
2025-06-09 15:19:57 +0200

    game script

---
+++ GravityMan/Assets/Script/Game.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private PlayerData player;
+    [SerializeField] private Obstacle[] obstacles;
+
+    private List<Obstacle> damageObstacles;
+
+    private void Start()
+    {
+        //set Player hp
+        player.Health = player.MaxHealth;
+
+        //spawn 5 of each added object type
+        foreach (var obstacle in obstacles)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                GameObject obj = Instantiate(obstacle.prefab);
+                float rnd = Random.Range(-4, 4);
+                obj.transform.position = new Vector3(0, rnd, 0);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        player.PlayerUpdate();
+    }
+}
